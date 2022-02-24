2a639fd7282c8c31a643c6549aec68facb27d008
Rohit Tyagi <92664353+MrRohitTyagi@users.noreply.github.com>
2022-02-24 15:53:08 +0530

    Add files via upload

---
+++ LC20. Valid Parentheses(Python sol)/main.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        while(("()" in s) or ("{}"in s) or ("[]" in s)):
+            if "()" in s :
+                s=s.replace("()","") 
+                
+            if "{}" in s:
+                s=s.replace("{}","")
+                
+            if "[]" in s:
+                s=s.replace("[]","")
+                
+        return s==""
+            
